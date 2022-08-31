948d210447bcdc8991cb199d2954332abce96603
Ралейно Денис Александрович <Raleyno@aebank.by>
2022-08-31 14:14:16 +0300

    Page Object - Рабочий вариант. Переходы по страницам

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")             # Есть ссылка для входа/регистрации
+    
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form")         # Есть форма для входа
+    REGISTER_FORM = (By.CSS_SELECTOR, "form#register_form")   # Есть форма регистрации
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 ﻿from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 #def go_to_login_page(browser):
@@ -6,9 +7,19 @@ from selenium.webdriver.common.by import By
 #    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-   
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    #login_page = page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    
+        
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+        
 	# pytest -v --tb=line --language=en test_main_page.py 	- запуск
